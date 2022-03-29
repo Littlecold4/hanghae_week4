308666c5b1656b4cce7387db4cdfcf856069509e
LIttlecold4 <minu09252@gmail.com>
2022-03-29 20:54:39 +0900

    two_testcase

--- src/test/java/com/sparta/week03/domain/UserTest.java
+++ src/test/java/com/sparta/week03/domain/UserTest.java
@@ -2,7 +2,6 @@ package com.sparta.week03.domain;
 
 import com.sparta.week03.Service.UserService;
 import com.sparta.week03.dto.SignupRequestDto;
-import com.sparta.week03.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -10,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
 
@@ -63,7 +62,7 @@ class UserTest {
             @DisplayName("아이디 오류")
             class Title {
                 @Test
-                @DisplayName("아이디 이름이 4자리 이하")
+                @DisplayName("아이디 이름이 4자리 이하 --(1)")
                 void fail1() {
                     // given
                     username = "ac";
@@ -91,10 +90,63 @@ class UserTest {
                 }
 
                 @Test
-                @DisplayName("이상한 문자")
+                @DisplayName("아이디 이름이 4자리 이하 --(2)")
                 void fail2() {
                     // given
+                    username = "a";
+
+                    SignupRequestDto requestDto = new SignupRequestDto(
+                            username,
+                            password,
+                            passwordCheck,
+                            email
+                    );
+
+                    // when
+//                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+//                        new User(requestDto, userId);
+//                    });
+                    Optional<User> found = Optional.empty();
+                    String message = null;
+                    try {
+                        UserService.UserIDCheck(password, username, passwordCheck, found);
+                    } catch (IllegalArgumentException e) {
+                        message = e.getMessage();
+                    }
+                    // then
+                    assertEquals("아이디는 최소 3자 이상이어야 합니다.", message);
+                }
+
+                @Test
+                @DisplayName("이상한 문자 --(1)")
+                void fail3() {
+                    // given
                     String username = "@#$$$$";
+
+                    SignupRequestDto requestDto = new SignupRequestDto(
+                            username,
+                            password,
+                            passwordCheck,
+                            email
+                    );
+
+                    // when
+                    Optional<User> found = Optional.empty();
+                    String message = null;
+                    try {
+                        UserService.UserIDCheck(password, username, passwordCheck, found);
+                    } catch (IllegalArgumentException e) {
+                        message = e.getMessage();
+                    }
+
+                    // then
+                    assertEquals("ID는 알파벳 대문자 소문자와 숫자만 입력 가능합니다", message);
+                }
+                @Test
+                @DisplayName("이상한 문자 --(2)")
+                void fail4() {
+                    // given
+                    String username = "@#$$123123$$";
 
                     SignupRequestDto requestDto = new SignupRequestDto(
                             username,
@@ -119,9 +171,9 @@ class UserTest {
 
             @Nested
             @DisplayName("PASSWORD")
-            class Image {
+            class password {
                 @Test
-                @DisplayName("3자리 이상")
+                @DisplayName("3자리 이상--(1)")
                 void fail1() {
                     // given
                     password = "12";
@@ -145,12 +197,64 @@ class UserTest {
                     // then
                     assertEquals("비밀번호는 4글자 이상이어야합니다", message);
                 }
-
                 @Test
-                @DisplayName("확인 비먼이 다름")
+                @DisplayName("3자리 이상--(2)")
                 void fail2() {
                     // given
-                    password = "1233";
+                    password = "1";
+
+                    SignupRequestDto requestDto = new SignupRequestDto(
+                            username,
+                            password,
+                            passwordCheck,
+                            email
+                    );
+
+                    // when
+                    Optional<User> found = Optional.empty();
+                    String message = null;
+                    try {
+                        UserService.UserIDCheck(password, username, passwordCheck, found);
+                    } catch (IllegalArgumentException e) {
+                        message = e.getMessage();
+                    }
+
+                    // then
+                    assertEquals("비밀번호는 4글자 이상이어야합니다", message);
+                }
+
+
+                @Test
+                @DisplayName("확인 비먼이 다름--(1)")
+                void fail3() {
+                    // given
+                    password = "12122233";
+                    passwordCheck = "12333";
+
+                    SignupRequestDto requestDto = new SignupRequestDto(
+                            username,
+                            password,
+                            passwordCheck,
+                            email
+                    );
+
+                    // when
+                    Optional<User> found = Optional.empty();
+                    String message = null;
+                    try {
+                        UserService.UserIDCheck(password, username, passwordCheck, found);
+                    } catch (IllegalArgumentException e) {
+                        message = e.getMessage();
+                    }
+
+                    // then
+                    assertEquals("비밀번호가 동일하지 않습니다.", message);
+                }
+                @Test
+                @DisplayName("확인 비먼이 다름--(1)")
+                void fail4() {
+                    // given
+                    password = "1333333";
                     passwordCheck = "12333";
 
                     SignupRequestDto requestDto = new SignupRequestDto(
